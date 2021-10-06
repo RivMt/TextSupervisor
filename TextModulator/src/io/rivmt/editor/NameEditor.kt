b25d4fbe759eef4e45a150d87d9778c90991b9e5
강산 <san.kang@outlook.com>
2021-10-06 07:12:14 +0000

    Fix NameEditor editing function

--- TextModulator/src/io/rivmt/editor/NameEditor.kt
+++ TextModulator/src/io/rivmt/editor/NameEditor.kt
@@ -7,22 +7,20 @@ import io.rivmt.language.name.NameObject
 class NameEditor {
     companion object {
         fun fixNameConsistency(list: MutableList<NameObject>, text: MutableList<String>): MutableList<String> {
-            val result = mutableListOf<String>()
+            val result = text
             
             Log.v("인명 일관성 검사")
             for(find in list) {
                 for(item in find.candidates) {
                     if (item != find.change) {
-                        val count = Utility.findWordInList(text)
+                        val count = Utility.findWordInList(item, text)
                         if (count > 0) {
                             Log.v("${find.input}에 대한 후보 ${item}에 대해 ${count}개가 발견되었습니다.")
                             Log.v("${find.change}로 수정하시겠습니까? (Y/n)")
                             val rl = readLine()
-                            for(line in text) {
+                            for((i,line) in text.withIndex()) {
                                 if (rl!!.toLowerCase() == "y") {
-                                    result.add(line.replace(item, find.change))
-                                } else {
-                                    result.add(line)
+                                    result[i] = line.replace(item, find.change)
                                 }
                             }
                         }
