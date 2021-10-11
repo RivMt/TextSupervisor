9e5e257ac75bdfd87bd8e98c7c0bb6593f6273a4
강산 <san.kang@outlook.com>
2021-10-11 01:43:02 +0000

    Enhance tag editor

--- BookEditor/src/io/rivmt/modulator/editor/TagEditor.kt
+++ BookEditor/src/io/rivmt/modulator/editor/TagEditor.kt
@@ -1,13 +1,34 @@
 package io.rivmt.modulator.editor
 
+import io.rivmt.utils.utility.Log
+
 class TagEditor {
     companion object {
         fun appendTag(text: MutableList<String>): MutableList<String> {
             val result = mutableListOf<String>()
             for (item in text) {
-                result.add("<p>" + item + "</p>")
+                //Get tag name
+                val tag = extractTagName(item)
+                
+                //
+                if (tag == "") { //Append default tag
+                    result.add("<p>" + item + "</p>")
+                } else { //Close tag
+                    result.add(item + "</$tag>")
+                }
             }
             return result
-        } 
+        }
+        
+        fun extractTagName(line: String): String {
+            val find = "<[a-z][a-z0-9]+>".toRegex()
+            var tag = ""
+            if (find.containsMatchIn(line)) {
+                val tail = ">.+".toRegex()
+                tag = tail.replace(line, "").replace("<", "")
+            }
+            Log.d("Tag found: $tag")
+            return tag
+        }
     }
 }
